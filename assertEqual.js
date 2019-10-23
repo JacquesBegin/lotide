9cff11b547a4a21724aa6c476b922e2542190046
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-22 21:58:51 -0400

    Adds emoji's to pass/fail output for easier viewing.

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("Assertion Failed");
+    console.log("✅ Assertion Failed");
   } else {
-    console.log("Assertion Passed");
+    console.log("⛔️ Assertion Passed");
   }
 };
 
