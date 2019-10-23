3693bbba4f0475ebe4a67f547028b1be1f7e8d90
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-22 21:53:14 -0400

    Implements simple logic for finding equality between to arguments.

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,9 @@
 const assertEqual = function(actual, expected) {
-
+  if (actual !== expected) {
+    console.log("Not a match");
+  } else {
+    console.log("They match");
+  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
