43a3a2bdc279def7ba2420f3664ddf46bf58e886
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-24 22:51:02 -0400

    Adds functionality to check individual elements of arrays.

--- tail.js
+++ tail.js
@@ -10,9 +10,38 @@ function tail(arr) {
 }
 
 
-equals(tail(["Lighthouse Labs", "Bootcamp", "Yay!"]), "Lighthouse Labs");
-equals(tail([1, 2, 3, 4]), 1);
-equals(tail(["Lighthouse Labs", "Bootcamp", "Yay!"]), ["Bootcamp", "Yay!"]);
-equals(tail([1, 2, 3, 4]), [2, 3, 4]);
-equals(tail([]));
+// equals(tail(["Lighthouse Labs", "Bootcamp", "Yay!"]), "Lighthouse Labs");
+// equals(tail([1, 2, 3, 4]), 1);
+// equals(tail(["Lighthouse Labs", "Bootcamp", "Yay!"]), ["Bootcamp", "Yay!"]);
+// equals(tail([1, 2, 3, 4]), [2, 3, 4]);
+// equals(tail([]));
 
+let arr1 = ["Lighthouse Labs", "Bootcamp", "Yay!"];
+let arr2 = [1, 2, 3, 4];
+let tailArr1 = tail(arr1);
+let tailArr2 = tail(arr2);
+let expectedArr1 = ["Bootcamp", "Yay!", "D"];
+let expectedArr2 = [2, 3, 4];
+
+let equal = true;
+// while (equal) {
+//   for (let x = 0; x < tailArr1.length; x++) {
+//     for (let y = 0; y < expectedArr1.length; y++) {
+//       if (equals(tailArr1[x], expectedArr1[y])) {
+//         equal = false;
+//         break;
+//       }
+//     }
+//   }
+//   break;
+// }
+if (tailArr1.length === expectedArr1.length) {
+  for (let x = 0; x < tailArr1.length; x++) {
+    equals(tailArr1[x], expectedArr1[x]) 
+  }
+} else {
+  console.log("Not equal lengths.");
+  equal = false;
+}
+
+console.log(`equals: ${equal}`);
