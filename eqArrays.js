593d388f4d441743b2382f0e3e788c02794d309f
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-27 20:58:42 -0400

    Creates a function to compare equality of arrays (not a deep scan).

---
+++ eqArrays.js
@@ -0,0 +1,13 @@
+const assertEqual = require("./assertEqual.js");
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false
+    }
+    return true;
+  }
+  return false;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true);
