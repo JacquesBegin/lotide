687bf860e6da33625828fc89e070a2f73a02ad5b
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-28 22:46:56 -0400

    Implements the flatten function with recrusion to handle "unlimited" levels of nested arrays.

--- flatten.js
+++ flatten.js
@@ -3,5 +3,15 @@ const assertArraysEqual = require("./assertArraysEqual.js");
 
 
 function flatten(arr) {
-  
+  let flattenedArray = [];
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      flattenedArray = flattenedArray.concat(flatten(element));
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
 }
+
+console.log(flatten([1, 2, [3, 4, [5, 6, [7, 8, 9, [10, 11, 12]]]], 13, 14]));
