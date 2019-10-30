9e978895928a15ac7a690ceb4a857ac6ff457f9c
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-29 21:29:51 -0400

    Creates test cases.

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,24 @@
 const assertEqual = require("./assertEqual.js");
 
 const countOnly = function(allItems, itemsToCount) {
-  
+
 }
+
+
+const firstNames = [
+  "Karl",
+  "Carl",
+  "Salimander",
+  "Algae",
+  "Fang",
+  "Koloth",
+  "Kang",
+  "Fang",
+  "Algae"
+]
+
+const result1 = countOnly(firstNames, {"Kang": true, "Dingo": true, "Fang": true});
+
+assertEqual(result1["Kang"], 1);
+assertEqual(result1["Dingo"], undefined);
+assertEqual(result1["Fang"], 2)
