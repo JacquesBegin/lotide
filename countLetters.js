041d999df1daeed0215f2bef22bd9460a3a67264
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-29 22:11:07 -0400

    Finishes countLetters function.

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,15 @@
 const assertEqual = require("./assertEqual.js");
 
 function countLetters(str) {
-  
+  let letterCount = {};
+  for (let letter of str) {
+    if (letterCount[letter]) {
+      letterCount[letter]++;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
 }
+
+assertEqual(countLetters("hello")["l"], 2);
