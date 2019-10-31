7f3c1d82aa53b0fccda0a5a1e243929d9569fa43
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-30 22:17:54 -0400

    Creates initial function to find object value.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,13 @@
+const assertArraysEqual = require("./assertArraysEqual.js");
 
-
-const findKey = function(value) {
-  
+const findKeyByValue = function(obj, value) {
+  for (const item of obj) {
+    if (obj[item] === value) return item;
+  }
+  return undefined;
 }
+
+
+}
+
+
