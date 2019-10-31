888ce16e97a60c0caa4cb33b680065855cbf4b91
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-30 23:06:22 -0400

    Implements eqObjects for primitive values only.

--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,16 @@
 const assertEqual = require("./assertEqual.js");
 
+// Only test equality for primitives not for nested objects!!
 const eqObjects = function(obj1, obj2) {
-  
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+
+  for(const item1 in obj1) {
+    if (!obj2[item1] || obj1[item1] !== obj2[item1]) return false;
+  }
+
+  return true;
 }
+
+console.log(eqObjects({"a": "a", "b": "b", "c": "c"}, {"a": "a", "b": "b"}));
