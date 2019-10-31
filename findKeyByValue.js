96564e4bba27c5fac985bfa980119db218b6a3f0
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-30 22:22:31 -0400

    Modifies for of loop to be for in loop and adds test case (passes).

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,7 +1,7 @@
 const assertEqual = require("./assertEqual.js");
 
 const findKeyByValue = function(obj, value) {
-  for (const item of obj) {
+  for (const item in obj) {
     if (obj[item] === value) return item;
   }
   return undefined;
@@ -13,6 +13,6 @@ const shows = {
   "drama": "Lame Movie"
 }
 
-assertEqual()
+assertEqual(findKeyByValue(shows, "Star Trek"), "sci-fi");
 
 
